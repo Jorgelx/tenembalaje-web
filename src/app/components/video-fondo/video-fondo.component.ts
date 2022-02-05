1adc8f8512a62c46e1ea3b94c80c5d19bafadddd
Jorgelx <50906528+Jorgelx@users.noreply.github.com>
2022-02-05 20:11:03 +0100

    he cfddfg n,

---
+++ src/app/components/video-fondo/video-fondo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-fondo',
+  templateUrl: './video-fondo.component.html',
+  styleUrls: ['./video-fondo.component.css']
+})
+export class VideoFondoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
